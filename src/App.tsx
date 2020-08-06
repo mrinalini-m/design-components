1f26e072d5a8177be32166aae7190fee3fe51248
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-08-06 10:58:31 -0700

    Add ant design table demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Table from './components/Table'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ padding: '2rem' }}>
+      <h3>Ant Design Table Example</h3>
+      <Table />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Table.tsx
@@ -0,0 +1,89 @@
+import { Table, Tag, Space } from 'antd'
+import React from 'react'
+
+export interface ColumnType<RecordType> {
+  dataIndex?: string
+  key: React.Key
+  title?: React.ReactNode
+  render?: (value: any, record: RecordType, index: number) => React.ReactNode
+}
+
+const TableExample = () => {
+  const columns: Array<ColumnType<Record<string, any>>> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text: string) => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (tags: string[]) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green'
+            if (tag === 'loser') {
+              color = 'volcano'
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            )
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text: string, record: { [propName: string]: any }) => (
+        <Space size='middle'>
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ]
+
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ]
+
+  return (
+    <Table style={{ padding: '1rem' }} columns={columns} dataSource={data} />
+  )
+}
+export default TableExample
